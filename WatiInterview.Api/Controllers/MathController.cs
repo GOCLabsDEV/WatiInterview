4f549ffc21b8af7ac45797a6b1aa8ff0fbeff0c1
GOC Labs <goc.iot@outlook.com>
2022-12-06 12:41:18 +0530

    Add project files.

---
+++ WatiInterview.Api/Controllers/MathController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WatiInterview.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpPost]
+        [Route("add")]
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
